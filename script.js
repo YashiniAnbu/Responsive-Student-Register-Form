ee7d4581954a243898c29bf621d652ff3eb8d11b
YashiniAnbu <yashinianbu2004@gmail.com>
2026-03-09 07:56:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("studentForm");
+const tableBody = document.querySelector("#dataTable tbody");
+     form.addEventListener("submit", function(event){
+        event.preventDefault();
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const address = document.getElementById("address").value;
+    const pincode = document.getElementById("pincode").value;
+    const state = document.getElementById("state").value;
+    const country = document.getElementById("country").value;
+    const gender = document.querySelector("input[name='gender']:checked").value;
+
+    const newRow = document.createElement("tr");
+newRow.innerHTML = `
+        <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${email}</td>
+    <td>${address}</td>
+    <td>${pincode}</td>
+    <td>${gender}</td>
+    <td>${state}</td>
+    <td>${country}</td>
+    `;
+
+tableBody.appendChild(newRow);
+form.reset();
+});
